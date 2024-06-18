62bae3ea20c4f1b8c7064401076ab4c94c1a51bd
Luciano Pesce <luchopesce96@gmail.com>
2024-06-18 15:16:35 -0300

    fix(auth.js): problema con autorizacion en vercel

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken';
-import { options } from '../config/options.js';
+  import jwt from 'jsonwebtoken';
+  import { options } from '../config/options.js';
 
-const authMiddleware = (req, res, next) => {
-  const cookieToken = req.cookies['user-session'];
-  const headerToken = req.headers['authorization']?.split(' ')[1];
+  const authMiddleware = (req, res, next) => {
+    const cookieToken = req.cookies['user-session'];
+    const headerToken = req.headers['authorization']?.split(' ')[1];
+/*
+    if (!cookieToken || !headerToken) {
+      return res.status(401).json({ message: 'Both tokens are required' });
+    }
 
-  if (!cookieToken || !headerToken) {
-    return res.status(401).json({ message: 'Both tokens are required' });
-  }
+    if (cookieToken !== headerToken) {
+      return res.status(401).json({ message: 'Tokens do not match' });
+    }
+      */
 
-  if (cookieToken !== headerToken) {
-    return res.status(401).json({ message: 'Tokens do not match' });
-  }
+    try {
+      const decoded = jwt.verify(headerToken, options.jwt.secret);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  };
 
-  try {
-    const decoded = jwt.verify(cookieToken, options.jwt.secret);
-    req.user = decoded.user;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
-};
-
-export default authMiddleware;
+  export default authMiddleware;
